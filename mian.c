b4efdceddaa6d92b531cc8a9c1ba293e673f2f36
manjunadh404 <manjunadhbodapati@gmail.com>
2025-03-07 12:04:38 +0530

    addied modifed main

--- mian.c
+++ mian.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<add.h>
-#include<sub.h>
-#include<mul.h>
+// #include<sub.h>
+// #include<mul.h>
 int main()
 {
     int a,b;
     printf("enter a,b values:");
     scanf("%d%d",&a,&b);
     printf("addtion is %d\n",add(a,b));
-    printf("addtion is %d\n",sub(a,b));
-    printf("addtion is %d\n",mul(a,b));
+    // printf("addtion is %d\n",sub(a,b));
+    // printf("addtion is %d\n",mul(a,b));
     return 0;
 }
